42c917994dd5e07d719f748b80a078871d4de006
sven1103 <sven.fillinger@qbic.uni-tuebingen.de>
2017-06-07 10:05:36 +0200

    Update version to 0.2dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dync',
-    version='0.1dev',
+    version='0.2dev',
     packages=['dync'],
     license='GPL2+',
     long_description=open('README.md').read(),
